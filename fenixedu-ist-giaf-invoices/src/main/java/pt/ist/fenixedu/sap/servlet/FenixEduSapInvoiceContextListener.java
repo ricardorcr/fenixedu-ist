d5c32b4a46b924e1242e1cf49147a99284312e4e
Luis Cruz <luis.cruz@tecnico.pt>
2018-07-25 12:33:32 +0100

    Add deletion blockers and simplify code.

--- fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/sap/servlet/FenixEduSapInvoiceContextListener.java
+++ fenixedu-ist-giaf-invoices/src/main/java/pt/ist/fenixedu/sap/servlet/FenixEduSapInvoiceContextListener.java
@@ -1,17 +1,22 @@
 package pt.ist.fenixedu.sap.servlet;
 
+import java.util.Collection;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
 import org.fenixedu.academic.domain.accounting.AccountingTransaction;
+import org.fenixedu.academic.domain.accounting.Discount;
 import org.fenixedu.academic.domain.accounting.Event;
 import org.fenixedu.academic.domain.accounting.EventState;
 import org.fenixedu.academic.domain.accounting.EventState.ChangeStateEvent;
+import org.fenixedu.academic.domain.accounting.Exemption;
 import org.fenixedu.bennu.core.signals.DomainObjectEvent;
 import org.fenixedu.bennu.core.signals.Signal;
 
 import pt.ist.fenixedu.giaf.invoices.ui.SapInvoiceController;
+import pt.ist.fenixframework.FenixFramework;
 
 @WebListener
 public class FenixEduSapInvoiceContextListener implements ServletContextListener {
@@ -20,6 +25,9 @@ public class FenixEduSapInvoiceContextListener implements ServletContextListener
     public void contextInitialized(final ServletContextEvent sce) {
         Signal.register(AccountingTransaction.SIGNAL_ANNUL, this::handlerAccountingTransactionAnnulment);
         Signal.register(EventState.EVENT_STATE_CHANGED, this::handlerEventStateChange);
+
+        FenixFramework.getDomainModel().registerDeletionBlockerListener(Exemption.class, this::blockExemption);
+        FenixFramework.getDomainModel().registerDeletionBlockerListener(Discount.class, this::blockDiscount);
     }
     
     @Override
@@ -37,21 +45,43 @@ public class FenixEduSapInvoiceContextListener implements ServletContextListener
         final Event event = eventStateChange.getEvent();
         final EventState oldtState = event.getEventState();
         final EventState newState = eventStateChange.getNewState();
-        if (oldtState == null) {
-            // Then it is a new event and nothing needs to be done.
-        } else if (oldtState == newState) {
+
+        /*
+         *  NewValue > |  null  | OPEN | CLOSED | CANCELED
+         *  OldValue   |________|______|________|__________
+         *     V       |  
+         *    null     |   OK   |  Ok  |   Ex   |   Ex
+         *    OPEN     |   Ex   |  OK  |   Ok   |   SAP
+         *   CLOSED    |   Ex   |  Ex  |   OK   |   Ex
+         *  CANCELED   |   Ex   |  Ex  |   Ex   |   OK
+         */
+
+        if (oldtState == newState) {
             // Not really a state change... nothing to be done.
-        } else if ((oldtState == EventState.CLOSED || oldtState == EventState.CANCELLED)
-                && (newState == EventState.CLOSED || newState == EventState.CANCELLED)) {
-            throw new Error("You should not cancel closed events or close cancelled events. Why do this? What is the point?");
-        } else if ((oldtState == EventState.CLOSED || oldtState == EventState.CANCELLED)) {
-            throw new Error("SAP does not allow reopening closed or cancelledd events. Consider creating a new event instead.");
-        } else if (oldtState == EventState.OPEN) {
+        } else if (oldtState == null && newState == EventState.OPEN) {
+            // Ack, normal SAP integration will be fine.
+        } else if (oldtState == EventState.OPEN && newState == EventState.CLOSED) {
+            // Ack, normal SAP integration will be fine.
+        } else if (oldtState == EventState.OPEN && newState == EventState.CANCELLED) {
             syncEvent(event);
             throw new Error("Event state change must first be canceled in SAP"); // TODO
         } else {
-            throw new Error("New event state that must be handled: " + oldtState.name());
+            throw new Error("New event state change that must be handled: "
+                    + (oldtState == null ? "" : oldtState.name()) + " -> "
+                    + (newState == null ? "null" : newState.name()));
         }
+    }
+
+    private void blockExemption(final Exemption exemption, final Collection<String> blockers) {
+        final Event event = exemption.getEvent();
+        syncEvent(event);
+        blockers.add("Exemption must be first undone in SAP"); // TODO
+    }
+
+    private void blockDiscount(final Discount discount, final Collection<String> blockers) {
+        final Event event = discount.getEvent();
+        syncEvent(event);
+        blockers.add("Discount must first be removed from SAP"); // TODO
     }
 
     private void syncEvent(final Event event) {
